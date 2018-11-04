15b5ff2b7c61ee566fedcca03cc32937bc63ad71
AndyMacKenzieDD <Lessthree@mail.ru>
2018-11-05 00:14:36 +0300

    Animation creating

---
+++ 3rd person/Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private CharacterController _charController;
+    private ControllerColliderHit _contact;
+    private Animator _animator;
+
+    public float rotSpeed = 5.0f;
+    public float moveSpeed = 6.0f;
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocisy = -10.0f;
+    public float minFall = -1.5f;
+
+    private float _vertSpeed;
+
+
+    void Start ()
+    {
+        _charController = GetComponent<CharacterController>();
+        _vertSpeed = minFall;
+        _animator = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        bool hitGround = false;
+        RaycastHit hit;
+        if(_vertSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            float check = (_charController.height + _charController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+
+        Vector3 movement = Vector3.zero;
+
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+
+        if (horInput != 0 || vertInput != 0)
+        {
+            movement.x = horInput * moveSpeed;
+            movement.z = vertInput * moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
+
+            _animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
+
+            movement *= Time.deltaTime;
+            _charController.Move(movement);
+
+            
+        }
+        
+
+        //if (_charController.isGrounded)
+        if (hitGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _vertSpeed = jumpSpeed;
+            }
+            else
+            {
+                _vertSpeed = minFall;
+                _animator.SetBool("Jumping", false);
+            }
+        }
+        else
+        {
+            _vertSpeed += gravity * 5 * Time.deltaTime;
+            if (_vertSpeed < terminalVelocisy)
+            {
+                _vertSpeed = terminalVelocisy;
+            }
+            if (_contact != null)
+            {
+                _animator.SetBool("Jumping", true);
+            }
+            if(_charController.isGrounded)
+            {
+                if(Vector3.Dot(movement, _contact.normal) < 0)
+                {
+                    movement = _contact.normal * moveSpeed;
+                }
+                else
+                {
+                    movement += _contact.normal * moveSpeed;
+                }
+            }
+        }
+
+        movement.y = _vertSpeed;
+        movement *= Time.deltaTime;
+        _charController.Move(movement);
+
+        
+	}
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        _contact = hit;
+    }
+}
