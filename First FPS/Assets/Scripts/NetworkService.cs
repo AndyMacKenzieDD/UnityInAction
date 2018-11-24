ddd5a810a3569cf9cfb6b32cfbef1d45c5ff92dc
AndyMacKenzieDD <Lessthree@mail.ru>
2018-11-24 16:11:24 +0300

    Get xml and JSON weather from openweathermap.org

---
+++ First FPS/Assets/Scripts/NetworkService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkService : MonoBehaviour
+{
+    private const string xmlApi = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&mode=xml&APPID=b5c83d3c8a729dc9c6f9578b8a9131be";
+    private const string jsonAPI = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&APPID=b5c83d3c8a729dc9c6f9578b8a9131be";
+
+    private bool IsResponseValid(WWW www)
+    {
+        if(www.error != null)
+        {
+            Debug.Log("bad connection");
+            return false;
+        }
+        else if(string.IsNullOrEmpty(www.text))
+        {
+            Debug.Log("bad data");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+        if (!IsResponseValid(www))
+        {
+            yield break;
+        }
+
+        callback(www.text);
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(xmlApi, callback);
+    }
+
+    public IEnumerator GetWeatherJSON(Action<string> callback)
+    {
+        return CallAPI(jsonAPI, callback);
+    }
+}
---
+++ First FPS/Assets/Scripts/WeatherManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml;
+using UnityEngine;
+using MiniJSON;
+using System.Collections.Generic;
+
+public class WeatherManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+    public float cloudValue { get; private set; }
+
+    private NetworkService _network;
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Weather manager starting...");
+        _network = service;
+        //StartCoroutine(_network.GetWeatherXML(OnXMLDataLoaded));
+        StartCoroutine(_network.GetWeatherJSON(OnJSONDataLoaded));
+        status = ManagerStatus.Started;
+    }
+
+    public void OnXMLDataLoaded(string data)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data);
+        XmlNode root = doc.DocumentElement;
+        
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100f;
+        Debug.Log("Value: " + cloudValue);
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
+
+        status = ManagerStatus.Started;
+    }
+
+    public void OnJSONDataLoaded(string data)
+    {
+        Dictionary<string, object> dict;
+        dict = Json.Deserialize(data) as Dictionary<string, object>;
+
+        Dictionary<string, object> clouds = (Dictionary<string, object>)dict["clouds"];
+        cloudValue = (long)clouds["all"] / 100f;
+
+        Debug.Log(cloudValue);
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
+
+        status = ManagerStatus.Started;
+    }
+        
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
