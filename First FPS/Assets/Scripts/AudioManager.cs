21be12572cc783e616bc0a5107618cbc548b1c0d
AndyMacKenzieDD <Lessthree@mail.ru>
2018-11-25 16:06:31 +0300

    Faded music swapping

--- First FPS/Assets/Scripts/AudioManager.cs
+++ First FPS/Assets/Scripts/AudioManager.cs
@@ -9,9 +9,16 @@ public class AudioManager : MonoBehaviour, IGameManager
     [SerializeField]
     private AudioSource music1Source;
     [SerializeField]
+    private AudioSource music2Source;
+    [SerializeField]
     private string introBGMusic;
     [SerializeField]
     private string levelBGMusic;
+    private AudioSource _activeMusic;
+    private AudioSource _inactiveMusic;
+
+    public float crossFadeRate = 1.5f;
+    private bool _crossFading;
 
     public ManagerStatus status { get; private set; }
 
@@ -49,9 +56,10 @@ public class AudioManager : MonoBehaviour, IGameManager
         {
             _musicVolume = value;
 
-            if(music1Source != null)
+            if(music1Source != null && !_crossFading)
             {
                 music1Source.volume = _musicVolume;
+                music2Source.volume = _musicVolume;
             }
         }
     }
@@ -71,6 +79,7 @@ public class AudioManager : MonoBehaviour, IGameManager
             if(music1Source != null)
             {
                 music1Source.mute = value;
+                music2Source.mute = value;
             }
         }
     }
@@ -85,8 +94,14 @@ public class AudioManager : MonoBehaviour, IGameManager
         _network = service;
         music1Source.ignoreListenerVolume = true;
         music1Source.ignoreListenerPause = true;
+        music2Source.ignoreListenerVolume = true;
+        music2Source.ignoreListenerPause = true;
 
         soundVolume = 1f;
+        musicVolume = 1f;
+
+        _activeMusic = music1Source;
+        _inactiveMusic = music2Source;
 
         status = ManagerStatus.Started;
     }
@@ -108,13 +123,46 @@ public class AudioManager : MonoBehaviour, IGameManager
 
     private void PlayMusic(AudioClip clip)
     {
-        music1Source.clip = clip;
-        music1Source.Play();
+        if(_crossFading)
+        {
+            return;
+        }
+        StartCoroutine(CrossFadeMusic(clip));
     }
 
     public void StopMusic()
     {
-        music1Source.Stop();
+        _activeMusic.Stop();
+        _inactiveMusic.Stop();
+    }
+
+    private IEnumerator CrossFadeMusic(AudioClip clip)
+    {
+        _crossFading = true;
+
+        _inactiveMusic.clip = clip;
+        _inactiveMusic.volume = 0;
+        _inactiveMusic.Play();
+
+        float scaleRate = crossFadeRate * _musicVolume;
+
+        while(_activeMusic.volume > 0)
+        {
+            _activeMusic.volume -= scaleRate * Time.deltaTime;
+            _inactiveMusic.volume += scaleRate * Time.deltaTime;
+
+            yield return null;
+        }
+
+        AudioSource temp = _activeMusic;
+
+        _activeMusic = _inactiveMusic;
+        _activeMusic.volume = _musicVolume;
+
+        _inactiveMusic = temp;
+        _inactiveMusic.Stop();
+
+        _crossFading = false;
     }
 
     void Start ()
